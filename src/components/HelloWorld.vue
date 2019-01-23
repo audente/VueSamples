b2b0f7011a26dcadb40a2c473b0a90d48b0ec2d3
Juan Valera <juan.valera@gmail.com>
2019-01-23 09:06:23 -0600

    rest

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,36 @@ export default {
   },
   methods: {
     run: function() {
-      alert("ok");
+      const url="https://ussouthcentral.services.azureml.net/workspaces/8a9e6c2473674cbda1a9047e2e1258e2/services/28c646b4a5a449cfae71eae39c1f7302/execute?api-version=2.0&details=true";
+      const auth="Bearer WBRsuaQ36vLeSR2iWDlLCB1gmPAO64N0G9vMJinQ2MWJfefqnAGFaxjWxxuL5JyqasL3VzpBHmp9QJLj3uX2Kw==";
+      const data = {"Inputs": {"input1": {
+        "ColumnNames": ["Indice", "NumCta", "Desc", "CodAgrup", "DescAgrup"],
+        "Values": [
+            ["1", "101", "Caja", "value1", "value2" ],
+            ["2", "201", "Capital", "value3", "value4" ]
+        ] } },
+        "GlobalParameters": {}
+      }
+      const json = JSON.stringify(data);
+      
+      alert("prepare");
+      
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+           if (this.readyState == 4 && this.status == 200) {
+               alert(this.responseText);
+           } else {
+             alert("FAILED:" + this.readyState + " - " this.status + " >> " + this.responseText);
+           }
+      };
+      xhttp.open("POST", url, true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.setRequestHeader("Authorization", auth);
+      xhttp.setRequestHeader("Host", "ussouthcentral.services.azureml.net");
+      xhttp.send(json);
+      
+      alert(("sent");
+
     }
   },
 };
